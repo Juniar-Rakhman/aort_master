189b3ca4607a8f60a399b4bfc721360991a8e46b
galihparyanto <galih.paryanto@gmail.com>
2016-08-16 15:00:03 +0700

    Fix spring-boot reactjs configuration and remove node module packages

--- src/main/java/nz/ac/ara/aort/controllers/WebController.java
+++ src/main/java/nz/ac/ara/aort/controllers/WebController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class WebController {
     
-    @RequestMapping(value="/")
+    @RequestMapping(value="/home")
     public String index() {
 
         return "index";
