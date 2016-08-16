3b5ca5ea30683b896fb9331a7a257b1d35ce23f4
juniar-rakhman <JuniarPrima.Rakhman@mitrais.com>
2016-08-16 18:01:02 +0800

    need to redo security added dummy data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,15 @@ INSERT INTO dbo.staff (id, first_name, last_name, username, email, office_phone,
 -- academic_observation.dbo.staff_position
 INSERT INTO dbo.staff_position (staff_id, position_id) VALUES ('00000065', '0000002166');
 INSERT INTO dbo.staff_position (staff_id, position_id) VALUES ('00000216', '0000002169');
+
+-- academic_observation.dbo.rating_reference
+INSERT INTO dbo.rating_reference (rating) VALUES ('A');
+INSERT INTO dbo.rating_reference (rating) VALUES ('AB');
+INSERT INTO dbo.rating_reference (rating) VALUES ('B');
+INSERT INTO dbo.rating_reference (rating) VALUES ('BC');
+INSERT INTO dbo.rating_reference (rating) VALUES ('C');
+INSERT INTO dbo.rating_reference (rating) VALUES ('D');
+INSERT INTO dbo.rating_reference (rating) VALUES ('E');
+
+-- academic_observation.dbo.observation
+INSERT INTO dbo.observation VALUES ('test', 1, 'test', '2016-07-11 00:00:00.0000000', 'test', 10, 'test', 1, 'test', 'test', 1, 'test', 10, 'test', 10, 'test', '2016-07-11 00:00:00.0000000', 10, NULL, NULL, NULL, NULL, NULL, NULL, 1, NULL);
