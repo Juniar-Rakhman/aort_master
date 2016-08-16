ac7b8544f83bb9695415d8ecfcec136d2d96febc
galihparyanto <galih.paryanto@gmail.com>
2016-08-16 08:54:57 +0700

    Frontend fixed

--- src/main/java/nz/ac/ara/aort/controllers/WebController.java
+++ src/main/java/nz/ac/ara/aort/controllers/WebController.java
@@ -11,8 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class WebController {
     
-    @RequestMapping("/")
+    @RequestMapping(value="/")
     public String index() {
+
         return "index";
     }
 }
