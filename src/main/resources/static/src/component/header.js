da0d4ebe2e287dcff6a9a089735f00a3dabebd5c
galihparyanto <galih.paryanto@gmail.com>
2016-08-18 15:57:52 +0700

    Update front page and react-scripts config

--- src/main/resources/static/src/component/header.js
+++ src/main/resources/static/src/component/header.js
@@ -10,15 +10,10 @@ class Header extends Component {
         <nav className="navbar navbar-static-top white-bg" role="navigation" style={style}>
           <div className="navbar-header">
             <a className="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i className="fa fa-bars"></i> </a>
-            <form role="search" className="navbar-form-custom" method="post" action="#">
-              <div className="form-group">
-                  <input type="text" placeholder="Search for something..." className="form-control" name="top-search" id="top-search" />
-              </div>
-            </form>
           </div>
           <ul className="nav navbar-top-links navbar-right">
             <li>
-              <a href="/logout">
+              <a href="login.html">
                   <i className="fa fa-sign-out"></i> Log out
               </a>
             </li>
