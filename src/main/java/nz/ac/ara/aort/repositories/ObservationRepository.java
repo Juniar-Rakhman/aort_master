6892e563d348ab20c5309be0a2022cb9d6cc79dd
juniar-rakhman <JuniarPrima.Rakhman@mitrais.com>
2016-08-19 10:39:03 +0800

    added observation search by staff date and observer

--- src/main/java/nz/ac/ara/aort/repositories/ObservationRepository.java
+++ src/main/java/nz/ac/ara/aort/repositories/ObservationRepository.java
@@ -2,10 +2,17 @@ package nz.ac.ara.aort.repositories;
 
 import nz.ac.ara.aort.entities.Observation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.sql.Date;
+import java.util.List;
 
 /**
  * Created by a9jr5626 on 8/12/16.
  */
 public interface ObservationRepository extends JpaRepository<Observation, Long> {
-    
+    List<Observation> findByStaff(@Param("staff") String staff);
+    List<Observation> findByDateBetween(@Param("after") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date after, @Param("before") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date before);
+    List<Observation> findByObserverPrimary(@Param("observerPrimary") String observerPrimary);
 }
