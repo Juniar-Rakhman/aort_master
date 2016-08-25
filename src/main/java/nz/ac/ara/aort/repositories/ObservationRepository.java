4829cbcbe69bc37311624a7a0d1e5adad5023c3c
juniar-rakhman <JuniarPrima.Rakhman@mitrais.com>
2016-08-25 18:08:12 +0800

    added searches by : - observer - line manager - hod - moderator - learning coach

--- src/main/java/nz/ac/ara/aort/repositories/ObservationRepository.java
+++ src/main/java/nz/ac/ara/aort/repositories/ObservationRepository.java
@@ -16,4 +16,9 @@ public interface ObservationRepository extends JpaRepository<Observation, Long> 
     List<Observation> findByStaffId(@Param("staffId") String staffId, Pageable pageable);
     List<Observation> findByDateBetween(@Param("after") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date after, @Param("before") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date before, Pageable pageable);
     List<Observation> findByObserverPrimaryId(@Param("observerPrimaryId") String observerPrimaryId, Pageable pageable);
+    List<Observation> findByObserverSecondaryId(@Param("observerSecondaryId") String observerSecondaryId, Pageable pageable);
+    List<Observation> findByLineManagerId(@Param("lineManagerId") String lineManagerId, Pageable pageable);
+    List<Observation> findByHodId(@Param("hodId") String hodId, Pageable pageable);
+    List<Observation> findByModeratorId(@Param("moderatorId") String moderatorId, Pageable pageable);
+    List<Observation> findByLearningCoachId(@Param("learningCoachId") String learningCoachId, Pageable pageable);
 }
