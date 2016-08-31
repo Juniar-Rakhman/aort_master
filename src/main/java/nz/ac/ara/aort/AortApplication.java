deb4bd0f3b9c790b0473307347047444b33ff2f4
juniar-rakhman <JuniarPrima.Rakhman@mitrais.com>
2016-08-31 19:46:46 +0800

    ready to upload to ARA server

--- src/main/java/nz/ac/ara/aort/AortApplication.java
+++ src/main/java/nz/ac/ara/aort/AortApplication.java
@@ -1,14 +1,18 @@
 package nz.ac.ara.aort;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AortApplication {
+public class AortApplication extends SpringBootServletInitializer {
 	
-	private static final Logger log = LoggerFactory.getLogger(AortApplication.class);
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AortApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AortApplication.class, args);
 	}
