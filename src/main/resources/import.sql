c1543d8670ac9608499b847a2c574ade920c7b76
juniar-rakhman <JuniarPrima.Rakhman@mitrais.com>
2016-09-05 13:51:02 +0800

    build to war 0.1.3

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -19,53 +19,42 @@
 -- INSERT INTO staff_position (staff_id, position_id) VALUES ('00000203', '0000002456');
 -- INSERT INTO staff_position (staff_id, position_id) VALUES ('00000242', '0000002123');
 
--- -- academic_observation.dbo.rating_reference
--- INSERT INTO dbo.rating_reference (rating) VALUES ('Excellent Practice');
--- INSERT INTO dbo.rating_reference (rating) VALUES ('Good Practice');
--- INSERT INTO dbo.rating_reference (rating) VALUES ('Practice Needs Improvement');
--- INSERT INTO dbo.rating_reference (rating) VALUES ('Practice Requires Intervention and Support');
--- 
--- -- academic_observation.dbo.strength_improvement_reference
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learner-centred teaching enables all learners to achieve', 'Learning strategies cater for the needs of the learners');
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learner-centred teaching enables all learners to achieve', 'Learning activities are varied and interesting');
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learner-centred teaching enables all learners to achieve', 'Learning time is managed effectively');
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learner-centred teaching enables all learners to achieve', 'Questioning techniques progress learning');
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learner-centred teaching enables all learners to achieve', 'Evidence of learner achievement');
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learning environments ensure participation and engagement', 'Learning environment is positive and respectful');
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learning environments ensure participation and engagement', 'Instructions, explanations and expectations are clear');
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learning environments ensure participation and engagement', 'Learning environment is well organised');
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learning environments ensure participation and engagement', 'Variety of interactive and independent activity');
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learning environments ensure participation and engagement', 'Good use of learning technologies');
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Cultural competence informs practice', 'Relationships are respectful ');
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Cultural competence informs practice', 'Teaching strategies are inclusive');
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Effective planning drives learner achievement', 'Teaching and learning plan has a clear structure');
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Effective planning drives learner achievement', 'Lesson flow, pace and level are appropriate');
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Effective planning drives learner achievement', 'In-class and self-directed learning activities are evident');
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Effective planning drives learner achievement', 'Activities make links to vocational/professional contexts');
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learners are well supported', 'Scaffolding and motivational techniques support progress');
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learners are well supported', 'Session relates to learning outcomes and to assessments');
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learners are well supported', 'Learning technologies and literacy/numeracy/academic literacies are evident');
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Feedback and assessment inform and improve student learning', 'In-class assessment and evaluation strategies are used to inform teaching');
--- INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Feedback and assessment inform and improve student learning', 'Constructive feedback is given to learners');
--- 
--- INSERT INTO dbo.observation VALUES ('testestsetst 1', 0, 'test aja 1', 1, 'test 1', 1, null, '2016-08-24', 'test 1', '00180224', 2, '00180224', null, null, '00180224', 'test 1', 1, '00180224', 'testsetstset1', '000180224', '00180224', 'estset1setse', 1, '2', 'te1stetsetse', 2, 'tests1et', '00180224', 1, '03:45:38.0000000', 1);
--- INSERT INTO dbo.observation VALUES ('testestsetst 2', 0, 'test aja 2', 1, 'test 2', 1, null, '2016-08-25', 'test 2', '00180224', 2, '00180224', null, null, '00180224', 'test 2', 1, '00180224', 'testsetstset2', '000180224', '00180224', 'estset2setse', 1, '2', 'te2stetsetse', 2, 'tests2et', '00180224', 1, '03:45:38.0000000', 1);
--- INSERT INTO dbo.observation VALUES ('testestsetst 3', 0, 'test aja 3', 1, 'test 3', 1, null, '2016-08-25', 'test 3', '00180224', 2, '00180224', null, null, '00180224', 'test 3', 1, '00180224', 'testsetstset3', '000180224', '00180224', 'estset3setse', 1, '2', 'te3stetsetse', 2, 'tests3et', '00180224', 1, '03:45:38.0000000', 1);
--- INSERT INTO dbo.observation VALUES ('testestsetst 4', 0, 'test aja 4', 1, 'test 4', 1, null, '2016-08-25', 'test 4', '00180224', 2, '00180224', null, null, '00180224', 'test 4', 1, '00180224', 'testsetstset4', '000180224', '00180224', 'estset4setse', 1, '2', 'te4stetsetse', 2, 'tests4et', '00180224', 1, '03:45:38.0000000', 1);
--- INSERT INTO dbo.observation VALUES ('testestsetst 5', 0, 'test aja 5', 1, 'test 5', 1, null, '2016-08-25', 'test 5', '00180224', 2, '00180224', null, null, '00180224', 'test 5', 1, '00180224', 'testsetstset5', '000180224', '00180224', 'estset5setse', 1, '2', 'te5stetsetse', 2, 'tests5et', '00180224', 1, '03:45:38.0000000', 1);
--- INSERT INTO dbo.observation VALUES ('testestsetst 6', 0, 'test aja 6', 1, 'test 6', 1, null, '2016-08-25', 'test 6', '00180224', 2, '00180224', null, null, '00180224', 'test 6', 1, '00180224', 'testsetstset6', '000180224', '00180224', 'estset6setse', 1, '2', 'te6stetsetse', 2, 'tests6et', '00180224', 1, '03:45:38.0000000', 1);
--- INSERT INTO dbo.observation VALUES ('testestsetst 7', 0, 'test aja 7', 1, 'test 7', 1, null, '2016-08-25', 'test 7', '00180224', 2, '00180224', null, null, '00180224', 'test 7', 1, '00180224', 'testsetstset7', '000180224', '00180224', 'estset7setse', 1, '2', 'te7stetsetse', 2, 'tests7et', '00180224', 1, '03:45:38.0000000', 1);
--- INSERT INTO dbo.observation VALUES ('testestsetst 8', 0, 'test aja 8', 1, 'test 8', 1, null, '2016-08-25', 'test 8', '00180224', 2, '00180224', null, null, '00180224', 'test 8', 1, '00180224', 'testsetstset8', '000180224', '00180224', 'estset8setse', 1, '2', 'te8stetsetse', 2, 'tests8et', '00180224', 1, '03:45:38.0000000', 1);
--- INSERT INTO dbo.observation VALUES ('testestsetst 9', 0, 'test aja 9', 1, 'test 9', 1, null, '2016-08-25', 'test 9', '00180224', 2, '00180224', null, null, '00180224', 'test 9', 1, '00180224', 'testsetstset9', '000180224', '00180224', 'estset9setse', 1, '2', 'te9stetsetse', 2, 'tests9et', '00180224', 1, '03:45:38.0000000', 1);
--- INSERT INTO dbo.observation VALUES ('testestsetst 10', 0, 'test aja 10', 1, 'test 10', 1, null, '2016-08-25', 'test 10', '00180224', 2, '00180224', null, null, '00180224', 'test 10', 1, '00180224', 'testsetstset10', '00180224', '00180224', 'estset10setse', 1, '2', 'te10stetsetse', 2, 'tests10et', '00180224', 1, '03:45:38.0000000', 1);
--- 
--- INSERT INTO academic_observation.dbo.staff (staff_id, department, email, first_name, is_employed, last_name, office_phone, total_fte, username) VALUES ('00180224','Department of Computing', 'Neo.Smith1@ara.ac.nz', 'Neo', 1, 'Smith1', '1984432418', 1.00, 'AcadObs01');
--- INSERT INTO academic_observation.dbo.staff (staff_id, department, email, first_name, is_employed, last_name, office_phone, total_fte, username) VALUES ('00180225','Learning Design Department', 'Morpheus.Smith2@ara.ac.nz', 'Morpheus', 1, 'Smith2', '467398155', 1.00, 'AcadObs02');
--- INSERT INTO academic_observation.dbo.staff (staff_id, department, email, first_name, is_employed, last_name, office_phone, total_fte, username) VALUES ('00180226','Human Resources Advisors', 'Trinity.Smith3@ara.ac.nz', 'Trinity', 1, 'Smith3', '1522353564', 0.80, 'AcadObs03');
--- INSERT INTO academic_observation.dbo.staff (staff_id, department, email, first_name, is_employed, last_name, office_phone, total_fte, username) VALUES ('00180227','English Language - EAR', 'Admin.Smith4@ara.ac.nz', 'Smith', 1, 'Smith4', '1627028273', 1.00, 'AcadObs04');
--- 
--- -- academic_observation.dbo.user_role
--- INSERT INTO dbo.user_role VALUES(0, 1, 0, '00180224', 0);
--- INSERT INTO dbo.user_role VALUES(1, 0, 0, '00180225', 0);
--- INSERT INTO dbo.user_role VALUES(0, 0, 1, '00180226', 0);
--- INSERT INTO dbo.user_role VALUES(0, 0, 0, '00180227', 1);
+INSERT INTO academic_observation.dbo.staff (staff_id, department, email, first_name, is_employed, last_name, office_phone, total_fte, username) VALUES ('00180224','Department of Computing', 'Neo.Smith1@ara.ac.nz', 'Neo', 1, 'Smith1', '1984432418', 1.00, 'ben');
+INSERT INTO academic_observation.dbo.staff (staff_id, department, email, first_name, is_employed, last_name, office_phone, total_fte, username) VALUES ('00180225','Learning Design Department', 'Morpheus.Smith2@ara.ac.nz', 'Morpheus', 1, 'Smith2', '467398155', 1.00, 'bob');
+INSERT INTO academic_observation.dbo.staff (staff_id, department, email, first_name, is_employed, last_name, office_phone, total_fte, username) VALUES ('00180226','Human Resources Advisors', 'Trinity.Smith3@ara.ac.nz', 'Trinity', 1, 'Smith3', '1522353564', 0.80, 'qauser');
+INSERT INTO academic_observation.dbo.staff (staff_id, department, email, first_name, is_employed, last_name, office_phone, total_fte, username) VALUES ('00180227','English Language - EAR', 'Admin.Smith4@ara.ac.nz', 'Smith', 1, 'Smith4', '1627028273', 1.00, 'admin');
+
+-- academic_observation.dbo.user_role
+INSERT INTO dbo.user_role VALUES(0, 1, 0, '00180224', 0);
+INSERT INTO dbo.user_role VALUES(1, 0, 0, '00180225', 0);
+INSERT INTO dbo.user_role VALUES(0, 0, 1, '00180226', 0);
+INSERT INTO dbo.user_role VALUES(0, 0, 0, '00180227', 1);
+
+-- academic_observation.dbo.rating_reference
+INSERT INTO dbo.rating_reference (rating) VALUES ('Excellent Practice');
+INSERT INTO dbo.rating_reference (rating) VALUES ('Good Practice');
+INSERT INTO dbo.rating_reference (rating) VALUES ('Practice Needs Improvement');
+INSERT INTO dbo.rating_reference (rating) VALUES ('Practice Requires Intervention and Support');
+
+-- academic_observation.dbo.strength_improvement_reference
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learner-centred teaching enables all learners to achieve', 'Learning strategies cater for the needs of the learners');
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learner-centred teaching enables all learners to achieve', 'Learning activities are varied and interesting');
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learner-centred teaching enables all learners to achieve', 'Learning time is managed effectively');
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learner-centred teaching enables all learners to achieve', 'Questioning techniques progress learning');
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learner-centred teaching enables all learners to achieve', 'Evidence of learner achievement');
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learning environments ensure participation and engagement', 'Learning environment is positive and respectful');
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learning environments ensure participation and engagement', 'Instructions, explanations and expectations are clear');
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learning environments ensure participation and engagement', 'Learning environment is well organised');
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learning environments ensure participation and engagement', 'Variety of interactive and independent activity');
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learning environments ensure participation and engagement', 'Good use of learning technologies');
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Cultural competence informs practice', 'Relationships are respectful ');
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Cultural competence informs practice', 'Teaching strategies are inclusive');
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Effective planning drives learner achievement', 'Teaching and learning plan has a clear structure');
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Effective planning drives learner achievement', 'Lesson flow, pace and level are appropriate');
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Effective planning drives learner achievement', 'In-class and self-directed learning activities are evident');
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Effective planning drives learner achievement', 'Activities make links to vocational/professional contexts');
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learners are well supported', 'Scaffolding and motivational techniques support progress');
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learners are well supported', 'Session relates to learning outcomes and to assessments');
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Learners are well supported', 'Learning technologies and literacy/numeracy/academic literacies are evident');
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Feedback and assessment inform and improve student learning', 'In-class assessment and evaluation strategies are used to inform teaching');
+INSERT INTO dbo.strength_improvement_reference (category, criteria) VALUES('Feedback and assessment inform and improve student learning', 'Constructive feedback is given to learners');
