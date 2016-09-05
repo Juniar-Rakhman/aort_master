1588d40bf607883f113b9e7becca8d113f83f154
juniar-rakhman <JuniarPrima.Rakhman@mitrais.com>
2016-09-05 13:58:42 +0800

    build to war 0.1.3

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,23 +1,23 @@
--- -- -- academic_observation.dbo.position
--- -- INSERT INTO position (position_id, manager_position_id, title, fte) VALUES ('0000002166', '0000001919', 'Kaiarahi Assistant', 1.00);
--- -- INSERT INTO position (position_id, manager_position_id, title, fte) VALUES ('0000002169', '0000001799', 'Student Events Team Member A', 1.00);
--- -- INSERT INTO position (position_id, manager_position_id, title, fte) VALUES ('0000002134', '0000001919', 'Student Events Team Member B', 1.00);
--- -- INSERT INTO position (position_id, manager_position_id, title, fte) VALUES ('0000002456', '0000001799', 'Student Events Team Member C', 1.00);
--- -- INSERT INTO position (position_id, manager_position_id, title, fte) VALUES ('0000002123', '0000001919', 'Student Events Team Member D', 1.00);
--- -- 
--- -- -- academic_observation.dbo.staff
--- -- INSERT INTO staff (staff_id, first_name, last_name,is_employed, campus_location, total_fte, username, email, office_phone, department) VALUES ('00000065', 'Richard', 'Smith1', true, 'not here', 99, 'Smith1R', 'Richard.Smith1@ara.ac.nz', '1984432418', 'Department of Computing');
--- -- INSERT INTO staff (staff_id, first_name, last_name,is_employed, campus_location, total_fte, username, email, office_phone, department) VALUES ('00000201', 'Selena', 'Smith2', true, 'not here', 99, 'Smith2S', 'Selena.Smith2@ara.ac.nz', '467398155', 'Learning Design Department');
--- -- INSERT INTO staff (staff_id, first_name, last_name,is_employed, campus_location, total_fte, username, email, office_phone, department) VALUES ('00000202', 'Bejo', 'Smith2', true, 'not here', 99, 'Smith2S', 'Bejo.Smith2@ara.ac.nz', '467398155', 'Learning Design Department');
--- -- INSERT INTO staff (staff_id, first_name, last_name,is_employed, campus_location, total_fte, username, email, office_phone, department) VALUES ('00000203', 'Paijo', 'Smith2', true, 'not here', 99, 'Smith2S', 'Paijo.Smith2@ara.ac.nz', '467398155', 'Learning Design Department');
--- -- INSERT INTO staff (staff_id, first_name, last_name,is_employed, campus_location, total_fte, username, email, office_phone, department) VALUES ('00000242', 'test', 'Smith2', true, 'not here', 99, 'Smith2S', 'test.Smith2@ara.ac.nz', '467398155', 'Learning Design Department');
--- -- 
--- -- -- academic_observation.dbo.staff_position
--- -- INSERT INTO staff_position (staff_id, position_id) VALUES ('00000065', '0000002166');
--- -- INSERT INTO staff_position (staff_id, position_id) VALUES ('00000201', '0000002169');
--- -- INSERT INTO staff_position (staff_id, position_id) VALUES ('00000202', '0000002134');
--- -- INSERT INTO staff_position (staff_id, position_id) VALUES ('00000203', '0000002456');
--- -- INSERT INTO staff_position (staff_id, position_id) VALUES ('00000242', '0000002123');
+-- -- academic_observation.dbo.position
+-- INSERT INTO position (position_id, manager_position_id, title, fte) VALUES ('0000002166', '0000001919', 'Kaiarahi Assistant', 1.00);
+-- INSERT INTO position (position_id, manager_position_id, title, fte) VALUES ('0000002169', '0000001799', 'Student Events Team Member A', 1.00);
+-- INSERT INTO position (position_id, manager_position_id, title, fte) VALUES ('0000002134', '0000001919', 'Student Events Team Member B', 1.00);
+-- INSERT INTO position (position_id, manager_position_id, title, fte) VALUES ('0000002456', '0000001799', 'Student Events Team Member C', 1.00);
+-- INSERT INTO position (position_id, manager_position_id, title, fte) VALUES ('0000002123', '0000001919', 'Student Events Team Member D', 1.00);
+-- 
+-- -- academic_observation.dbo.staff
+-- INSERT INTO staff (staff_id, first_name, last_name,is_employed, campus_location, total_fte, username, email, office_phone, department) VALUES ('00000065', 'Richard', 'Smith1', true, 'not here', 99, 'Smith1R', 'Richard.Smith1@ara.ac.nz', '1984432418', 'Department of Computing');
+-- INSERT INTO staff (staff_id, first_name, last_name,is_employed, campus_location, total_fte, username, email, office_phone, department) VALUES ('00000201', 'Selena', 'Smith2', true, 'not here', 99, 'Smith2S', 'Selena.Smith2@ara.ac.nz', '467398155', 'Learning Design Department');
+-- INSERT INTO staff (staff_id, first_name, last_name,is_employed, campus_location, total_fte, username, email, office_phone, department) VALUES ('00000202', 'Bejo', 'Smith2', true, 'not here', 99, 'Smith2S', 'Bejo.Smith2@ara.ac.nz', '467398155', 'Learning Design Department');
+-- INSERT INTO staff (staff_id, first_name, last_name,is_employed, campus_location, total_fte, username, email, office_phone, department) VALUES ('00000203', 'Paijo', 'Smith2', true, 'not here', 99, 'Smith2S', 'Paijo.Smith2@ara.ac.nz', '467398155', 'Learning Design Department');
+-- INSERT INTO staff (staff_id, first_name, last_name,is_employed, campus_location, total_fte, username, email, office_phone, department) VALUES ('00000242', 'test', 'Smith2', true, 'not here', 99, 'Smith2S', 'test.Smith2@ara.ac.nz', '467398155', 'Learning Design Department');
+-- 
+-- -- academic_observation.dbo.staff_position
+-- INSERT INTO staff_position (staff_id, position_id) VALUES ('00000065', '0000002166');
+-- INSERT INTO staff_position (staff_id, position_id) VALUES ('00000201', '0000002169');
+-- INSERT INTO staff_position (staff_id, position_id) VALUES ('00000202', '0000002134');
+-- INSERT INTO staff_position (staff_id, position_id) VALUES ('00000203', '0000002456');
+-- INSERT INTO staff_position (staff_id, position_id) VALUES ('00000242', '0000002123');
 -- 
 -- INSERT INTO academic_observation.dbo.staff (staff_id, department, email, first_name, is_employed, last_name, office_phone, total_fte, username) VALUES ('00180224','Department of Computing', 'Neo.Smith1@ara.ac.nz', 'Neo', 1, 'Smith1', '1984432418', 1.00, 'ben');
 -- INSERT INTO academic_observation.dbo.staff (staff_id, department, email, first_name, is_employed, last_name, office_phone, total_fte, username) VALUES ('00180225','Learning Design Department', 'Morpheus.Smith2@ara.ac.nz', 'Morpheus', 1, 'Smith2', '467398155', 1.00, 'bob');
