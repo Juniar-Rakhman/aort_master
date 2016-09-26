711686c125a2476288ef5c406239525d5c4c693a
Fathoni <akhmad.fathoni@mitrais.com>
2016-09-26 10:40:48 +0700

    container.js - passing role when create mode

--- src/main/resources/static/src/component/container.js
+++ src/main/resources/static/src/component/container.js
@@ -97,7 +97,7 @@ class Container extends Component {
     if (this.state.page === '') {
       content = <Home />;
     } else if (this.state.page === 'entry') {
-      content = <Entry title='Create' observation={this.state.data} staff={this.state.staff} redirectTo={this.handlePageNav}/>;
+      content = <Entry title='Create' observation={this.state.data} staff={this.state.staff} redirectTo={this.handlePageNav} role={this.state.role}/>;
     } else if (this.state.page === 'staffSearch') {
       content = <StaffSearch />
     } else if (this.state.page === 'observationSearch') {
