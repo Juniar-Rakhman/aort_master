fefd53457ed6d0bbf6bec91c9045a7cc324c2eda
Fathoni <akhmad.fathoni@mitrais.com>
2016-09-27 13:59:46 +0700

    observationSearch.js - Added search criteria form for observation search

--- src/main/resources/static/src/component/observationSearch.js
+++ src/main/resources/static/src/component/observationSearch.js
@@ -61,42 +61,173 @@ class SearchBar extends Component {
     constructor(props){
         super(props);
         this.handleChange = this.handleChange.bind(this);
-        this.state = {inputTextVal: ''}
+        var date = moment().format('DD/MM/YYYY');
+        this.state = {staff: props.searchCriteria.staff || '',
+            leadObserver: props.searchCriteria.leadObserver || '',
+            date: props.searchCriteria.date != null ? moment(props.searchCriteria.date).format('DD/MM/YYYY') : date,
+            completed: props.searchCriteria.completed || '',
+            lead: {firstName: '', lastName:''},
+            teacher: {firstName:'',lastName:''},
+
+        }
     }
 
     handleChange(e) {
-        this.props.onUserInput(
-            this.refs.filterTextInput.value
-        )
+        this.props.onUserInput()
         e.preventDefault();
     }
 
-    handleTextChange(e){
-        this.setState({inputTextVal: e.target.value});
+    handleTeacherChange(e) {
+        var newState = Object.assign(this.state, {staff: e.target.value});
+        this.setState({staff: e.target.value})
+        this.props.updateSearchCriteria(newState);
     }
+
+    handleLeadChange(e) {
+        var newState = Object.assign(this.state, {leadObserver: e.target.value});
+        this.setState({leadObserver: e.target.value})
+        this.props.updateSearchCriteria(newState);
+    }
+
+    handleDateChange(e) {
+        var newState = Object.assign(this.state, {date: e.target.childNodes[0].value});
+        this.setState({date: e.target.childNodes[0].value});
+        this.props.updateSearchCriteria(newState);
+    }
+
+    handleStatusChange(e) {
+        var newState = Object.assign(this.state, {completed: e.target.value});
+        this.setState({completed: e.target.value})
+        this.props.updateSearchCriteria(newState);
+    }
+
+  componentDidMount() {
+    var configs =
+       {
+        ajax: {
+          url: 'api/staffs/search/findByStaffName',
+          data: function(params){
+              return {
+                  name: params.term
+              }
+          },
+          processResults: function (data) {
+              return {
+                  results: data["_embedded"]["staffs"]
+              }
+          },
+          delay: 250
+        },
+        templateResult: function(staff){
+          return staff.firstName + " " + staff.lastName;
+        },
+        templateSelection: function formatRepoSelection (staff) {
+          if(staff===undefined){
+            return "Please select";
+          }
+          else{
+            if(staff.firstName != null){
+              return staff.firstName + " " + staff.lastName;
+            }
+            else if(staff.name != null){
+              return staff.name;
+            }
+            return "Please select";
+          }
+        },
+        initSelection: function(element, callback) {
+          var value = $(element).attr('data-init');
+          callback({name: value});
+        }
+      };
+
+    $('#teacher').select2(configs);
+    $('#teacher').on('change', this.handleTeacherChange.bind(this));
+
+    $('#lead').select2(configs);
+    $('#lead').on('change', this.handleLeadChange.bind(this));
+
+    $('#datePicker').datetimepicker({
+        format: 'DD/MM/YYYY'
+    });
+    $('#datePicker').on('dp.change', this.handleDateChange.bind(this)).trigger('dp.change');
+  }
     render() {
         var style = {
           "margin-bottom": "0px"
         }
         return (
-
-            <div className="m-b">
+          <div className="panel panel-default">
+            <div className="panel-heading">Search Criteria</div>
+            <div className="panel-body">
                 <form role="form" className="form-inline" onSubmit={this.handleChange.bind(this)}>
-                    <div className="form-group">
-                        <input type="text" placeholder="Find Observation" name="search" className="form-control"
-                            ref="filterTextInput"
-                            onChange={this.handleTextChange.bind(this)}/> &nbsp;
+
+                  <div className="col-lg-6">
+                    <label className="col-sm-4 control-label">Teacher's Name</label>
+                    <div className="col-sm-8 m-b">
+                        <select id="teacher"
+                          className="form-control"
+                          style={{width: "100%"}}
+                          data-init={this.state.teacher.firstName + ' ' + this.state.teacher.lastName}
+                          value={this.state.staff}>
+                            <option></option>
+                        </select>
                     </div>
+                  </div>
 
-                    <button
-                        style={style}
-                        className="btn btn-sm btn-primary"
-                        type="submit"
-                        disabled={this.state.inputTextVal === ''}>Search
-                    </button>
+                  <div className="col-lg-6">
+                    <label className="col-sm-4 control-label">Lead Observer</label>
+                    <div className="col-sm-8 m-b">
+                        <select id="lead"
+                          className="form-control"
+                          style={{width: "100%"}}
+                          data-init={this.state.lead.firstName + ' ' + this.state.lead.lastName}
+                          value={this.state.leadObserver}>
+                            <option></option>
+                        </select>
+                    </div>
+                  </div>
 
+                  <div className="col-lg-6">
+                    <label className="col-sm-4 control-label">Date</label>
+                    <div className="col-sm-8 m-b">
+                    <div className="input-group date" id="datePicker">
+                      <input type="text" className="form-control" value={this.state.date} />
+                      <span className="input-group-addon">
+                        <i className="fa fa-calendar" aria-hidden="true"></i>
+                      </span>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div className="col-lg-6">
+                    <label className="col-sm-4 control-label">Status</label>
+                    <div className="col-sm-8">
+                      <select
+                        className="form-control m-b"
+                        onChange={this.handleStatusChange.bind(this)}
+                        style={{width: "100%"}}>
+                        <option></option>
+                        <option value='false'>Open</option>
+                        <option value='true'>Completed</option>
+                      </select>
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12">
+                      <div className="col-sm-4">
+                      </div>
+                      <div className="col-sm-8">
+                          <button
+                            style={style}
+                            className="btn btn-sm btn-primary pull-right"
+                            type="submit">Search
+                          </button>
+                      </div>
+                   </div>
                 </form>
             </div>
+          </div>
         );
     }
 }
@@ -113,16 +244,37 @@ class ObservationSearch extends Component {
             filterText: '',
             page: 0,
             size: 10,
-            totalPages: 10
+            totalPages: 10,
+            searchCriteriaData: {}
         };
+
+        this.updateSearchCriteria = this.updateSearchCriteria.bind(this);
     }
 
-    handleUserInput(value){
-        this.setState({
-            page: 0,
-            filterText: value
+    handleUserInput(e){
+        var formattedDate = Object.assign(moment(this.state.searchCriteriaData.date, 'DD/MM/YYYY').format('YYYY-MM-DD'));
+        var data = Object.assign(this.state.searchCriteriaData, {date: formattedDate});
+        this.getObservationBySearchCriteria(data)
+    }
+
+    getObservationBySearchCriteria(searchCriteria){
+        var data = JSON.stringify(searchCriteria);
+        console.log(data);
+        $.ajax ({
+            type: 'POST',
+            url: '/api/observations/search?page=' + this.state.page + '&size=' + this.state.size,
+            contentType: "application/json",
+            data: data,
+            success: function (response) {
+                this.setState ({
+                    observations: response['content'],
+                    totalPages: response['totalPages']
+                });
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
         });
-        this.getObservationBySearch(value);
     }
 
     getObservationBySearch(filter){
@@ -180,6 +332,11 @@ class ObservationSearch extends Component {
         }
     }
 
+    updateSearchCriteria(data) {
+        var newCriteria = Object.assign(this.state.searchCriteriaData, data)
+        this.setState({searchCriteriaData: newCriteria});
+    }
+
     render() {
         var errorMessage = null;
         if(this.props.errorMessage != null) {
@@ -203,6 +360,8 @@ class ObservationSearch extends Component {
                                 <div className="table-responsive">
                                     <SearchBar
                                         onUserInput={this.handleUserInput}
+                                        updateSearchCriteria = {this.updateSearchCriteria}
+                                        searchCriteria = {this.state.searchCriteriaData}
                                     />
                                     <ObservationTable 
                                         observations={this.state.observations}
@@ -225,282 +384,5 @@ class ObservationSearch extends Component {
         );
     }
 }
-
-/*
-var OBSERVATIONS =
-[
-      {
-        "id" : 1,
-        "date" : "2016-08-17",
-        "time" : "11:00:00",
-        "lateLearners" : 1,
-        "location" : "test loc",
-        "moderated" : true,
-        "programme" : "test aja",
-        "programmeLevel" : 1,
-        "notes" : "test notest",
-        "ratingSummary" : "1",
-        "registeredLearners" : 15,
-        "sessionContext" : "test session",
-        "startLearners" : 11,
-        "strengthsShare" : "test str",
-        "totalLearners" : 12,
-        "additionalComments" : "test add comments",
-        "courseLevel" : 3,
-        "courseName" : "test aja",
-        "department" : "test dep",
-        "staff": {
-          "id" : "00000065",
-          "department": "Department of Computing",
-          "email": "Richard.Smith1@ara.ac.nz",
-          "firstName": "Richard",
-          "lastName": "Smith1",
-          "location": null,
-          "officePhone": "1984432418",
-          "username": "Smith1R"
-        },
-        "hod": {
-          "id" : "00000065",
-          "department": "Department of Computing",
-          "email": "Richard.Smith1@ara.ac.nz",
-          "firstName": "Richard",
-          "lastName": "Smith1",
-          "location": null,
-          "officePhone": "1984432418",
-          "username": "Smith1R"
-        },
-        "learningCoach": {
-          "id" : "00000065",
-          "department": "Department of Computing",
-          "email": "Richard.Smith1@ara.ac.nz",
-          "firstName": "Richard",
-          "lastName": "Smith1",
-          "location": null,
-          "officePhone": "1984432418",
-          "username": "Smith1R"
-        },
-        "lineManager": {
-          "id" : "00000065",
-          "department": "Department of Computing",
-          "email": "Richard.Smith1@ara.ac.nz",
-          "firstName": "Richard",
-          "lastName": "Smith1",
-          "location": null,
-          "officePhone": "1984432418",
-          "username": "Smith1R"
-        },
-        "observerPrimary": {
-          "id" : "00000065",
-          "department": "Department of Computing",
-          "email": "Richard.Smith1@ara.ac.nz",
-          "firstName": "Richard",
-          "lastName": "Smith1",
-          "location": null,
-          "officePhone": "1984432418",
-          "username": "Smith1R"
-        },
-        "moderator": {
-          "id" : "00000065",
-          "department": "Department of Computing",
-          "email": "Richard.Smith1@ara.ac.nz",
-          "firstName": "Richard",
-          "lastName": "Smith1",
-          "location": null,
-          "officePhone": "1984432418",
-          "username": "Smith1R"
-        },
-        "observerSecondary": {
-          "id" : "00000065",
-          "department": "Department of Computing",
-          "email": "Richard.Smith1@ara.ac.nz",
-          "firstName": "Richard",
-          "lastName": "Smith1",
-          "location": null,
-          "officePhone": "1984432418",
-          "username": "Smith1R"
-        },
-        "strengthImprovements": [
-            {
-              "id" : 1,
-              "improvement": true,
-              "strength": false,
-              "evidence": "test1",
-              "strImpRef": {
-                "id" : 1,
-                "category": "Learner-centred teaching enables all learners to achieve",
-                "criteria": "Learning strategies cater for the needs of the learners"
-              }
-            },
-            {
-              "id" : 2,
-              "improvement": false,
-              "strength": true,
-              "evidence": "test2",
-              "strImpRef": {
-                "id" : 2,
-                "category": "Learner-centred teaching enables all learners to achieve",
-                "criteria": "Learning activities are varied and interesting"
-              }
-            }
-        ],
-        "ratingReference": {
-          "id" : 1,
-          "rating": "Excellent"
-        },
-        "_links" : {
-          "self" : {
-            "href" : "http://localhost:8080/api/observations"
-          },
-          "profile" : {
-            "href" : "http://localhost:8080/api/profile/observations"
-          },
-          "search" : {
-            "href" : "http://localhost:8080/api/observations/search"
-          }
-        },
-        "page" : {
-          "size" : 20,
-          "totalElements" : 1,
-          "totalPages" : 1,
-          "number" : 0
-        }
-      },
-      {
-        "id": 2,
-        "date": "2016-08-17",
-        "time": "11:00:00",
-        "lateLearners": 1,
-        "location": "test loc",
-        "moderated": true,
-        "programme": "test aja",
-        "programmeLevel": 1,
-        "notes": "test notest",
-        "ratingSummary": "1",
-        "registeredLearners": 15,
-        "sessionContext": "test session",
-        "startLearners": 11,
-        "strengthsShare": "test str",
-        "totalLearners": 12,
-        "additionalComments": "test add comments",
-        "courseLevel": 3,
-        "courseName": "test aja",
-        "department": "test dep",
-        "staff": {
-          "id" : "00000065",
-          "department": "Department of Computing",
-          "email": "Richard.Smith1@ara.ac.nz",
-          "firstName": "Richard",
-          "lastName": "Smith1",
-          "location": null,
-          "officePhone": "1984432418",
-          "username": "Smith1R"
-        },
-        "hod": {
-          "id" : "00000065",
-          "department": "Department of Computing",
-          "email": "Richard.Smith1@ara.ac.nz",
-          "firstName": "Richard",
-          "lastName": "Smith1",
-          "location": null,
-          "officePhone": "1984432418",
-          "username": "Smith1R"
-        },
-        "learningCoach": {
-          "id" : "00000065",
-          "department": "Department of Computing",
-          "email": "Richard.Smith1@ara.ac.nz",
-          "firstName": "Richard",
-          "lastName": "Smith1",
-          "location": null,
-          "officePhone": "1984432418",
-          "username": "Smith1R"
-        },
-        "lineManager": {
-          "id" : "00000065",
-          "department": "Department of Computing",
-          "email": "Richard.Smith1@ara.ac.nz",
-          "firstName": "Richard",
-          "lastName": "Smith1",
-          "location": null,
-          "officePhone": "1984432418",
-          "username": "Smith1R"
-        },
-        "observerPrimary": {
-          "id" : "00000065",
-          "department": "Department of Computing",
-          "email": "Richard.Smith1@ara.ac.nz",
-          "firstName": "Richard",
-          "lastName": "Smith1",
-          "location": null,
-          "officePhone": "1984432418",
-          "username": "Smith1R"
-        },
-        "moderator": {
-          "id" : "00000065",
-          "department": "Department of Computing",
-          "email": "Richard.Smith1@ara.ac.nz",
-          "firstName": "Richard",
-          "lastName": "Smith1",
-          "location": null,
-          "officePhone": "1984432418",
-          "username": "Smith1R"
-        },
-        "observerSecondary": {
-          "id" : "00000065",
-          "department": "Department of Computing",
-          "email": "Richard.Smith1@ara.ac.nz",
-          "firstName": "Richard",
-          "lastName": "Smith1",
-          "location": null,
-          "officePhone": "1984432418",
-          "username": "Smith1R"
-        },
-        "strengthImprovements": [
-            {
-              "id": 1,
-              "improvement": true,
-              "strength": true,
-              "evidence": "test1",
-              "strImpRef": {
-                "id": 1,
-                "category": "Learner-centred teaching enables all learners to achieve",
-                "criteria": "Learning strategies cater for the needs of the learners"
-              }
-            },
-            {
-              "id": 2,
-              "improvement": false,
-              "strength": false,
-              "evidence": "test2",
-              "strImpRef": {
-                "category": "Learner-centred teaching enables all learners to achieve",
-                "criteria": "Learning activities are varied and interesting"
-              }
-            }
-        ],
-        "ratingReference": {
-          "id": 2,
-          "rating": "Good"
-        },
-        "_links" : {
-          "self" : {
-            "href" : "http://localhost:8080/api/observations"
-          },
-          "profile" : {
-            "href" : "http://localhost:8080/api/profile/observations"
-          },
-          "search" : {
-            "href" : "http://localhost:8080/api/observations/search"
-          }
-        },
-        "page" : {
-          "size" : 20,
-          "totalElements" : 1,
-          "totalPages" : 1,
-          "number" : 0
-        }
-      }
-    ];
-*/
 
 export default ObservationSearch;
