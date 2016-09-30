8edc093ed478a9d942c65d970dfea44db67dcb01
juniar-rakhman <JuniarPrima.Rakhman@mitrais.com>
2016-09-30 14:36:36 +0800

    merge from mitrais dev build no 0.2.1

--- src/main/java/nz/ac/ara/aort/WebSecurityConfig.java
+++ src/main/java/nz/ac/ara/aort/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package nz.ac.ara.aort;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
